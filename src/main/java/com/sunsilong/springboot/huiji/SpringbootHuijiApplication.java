7b612a7f13a7db71dfbd186f718a0ffdff653edd
sunsilong <17611360505@163.com>
2019-06-13 11:58:09 +0800

    first commit

---
+++ src/main/java/com/sunsilong/springboot/huiji/SpringbootHuijiApplication.java
@@ -0,0 +1,13 @@
+package com.sunsilong.springboot.huiji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHuijiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootHuijiApplication.class, args);
+    }
+
+}
